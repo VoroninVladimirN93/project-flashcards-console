8542faf5f0d296277c83ec09d778bed57eac7de3
YuliaToros <yulia.elb@gmail.com>
2024-10-25 11:58:10 +0300

    update ParseFile

--- ParseFile.js
+++ ParseFile.js
@@ -1,14 +1,20 @@
 const ReadFile = require ('./ReadFile')
 const {EOL} = require ('os')
+const QA = require ('./QA')
 
 class ParseFile {
 
     static async parse (path, code) {
         const getText = await ReadFile.getFile(path, code)
+
         const getArrayQA = getText.split(EOL).filter((el) => el!=(''))
+        console.log(getArrayQA)
+
         const arrayQA = getArrayQA.map (el => {
-            const [question, answer] =  el.split(',')
-            return new QA()})
+            const [question, answer] = el.split(EOL)
+            return new QA(question, answer)})    
+
+        console.log (arrayQA)
     }    
 
 }
